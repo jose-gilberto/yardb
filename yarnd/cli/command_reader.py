183214efc2d946fa0a035e8583df8b3589b3bf78
Gilberto Barbosa <medeiros.gilberto.br@gmail.com>
2021-09-12 03:16:51 -0300

    Add statements support

--- yarnd/cli/command_reader.py
+++ yarnd/cli/command_reader.py
@@ -1,4 +1,31 @@
 
+
+def exit_command():
+    exit(0)
+
+
+def unrecognized_command(input_buffer):
+    print(f'Unrecognized command \'{input_buffer}\'.')
+
+
+READABLE_COMMANDS = {
+    '.exit': exit_command
+}
+
+
+def execute_select():
+    print('SELECT')
+
+
+def execute_insert():
+    print('INSERT')
+
+
+READABLE_STATEMENTS = {
+    'select': execute_select,
+    'insert': execute_insert
+}
+
 class CommandReader:
 
     def __init__(self):
@@ -6,8 +33,22 @@ class CommandReader:
 
     def run(self):
         while True:
-            input_buffer = input('yardb>> ')
-            if input_buffer == '.exit':
-                exit(0)
+            input_buffer = input('yardb >> ')
+            
+            if input_buffer[0] == '.':
+                if input_buffer not in READABLE_COMMANDS:
+                    unrecognized_command(input_buffer)
+                    continue
+                READABLE_COMMANDS[input_buffer]()
+                continue
+
+            if input_buffer in READABLE_STATEMENTS:
+                READABLE_STATEMENTS[input_buffer]()
+                continue
             else:
-                print(f'Unrecognized command \'{input_buffer}\'.')
+                print(f'Unrecognized keyword at start of {input_buffer}.')
+                continue
+
+
+if __name__ == '__main__':
+    CommandReader().run()
