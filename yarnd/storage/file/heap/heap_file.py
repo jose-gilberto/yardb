23685193132424afa82a5439031b561e2b8d6456
Gilberto Barbosa <medeiros.gilberto.br@gmail.com>
2021-09-13 20:31:39 -0300

    Change page to interface and heappage

---
+++ yarnd/storage/file/heap/heap_file.py
@@ -0,0 +1,104 @@
+import sys
+
+
+class HeapPage:
+    # default size of a page in bytes
+    PAGE_SIZE: int = 8192
+
+    # pointer to the last line pointer
+    pd_lower: int
+    # pointer to the last tuple (on top)
+    pd_upper: int
+    # line pointers
+    line_pointers: list
+    # tuples
+    tuples: list
+
+    def __init__(self):
+        pass
+
+    def mount_page():
+        """
+        #####################################
+        #           PAGE STRUCTURE          #
+        #####################################
+
+        # [16 bits]
+        # pd_lower [8 bits]
+        # pd_upper [8 bits]
+
+        # [size - 16 bits]
+        
+        E.g.: Insert one tuple
+        #####################################
+        # [32] [-1] | [-1]                  #
+        #                                   #
+        #                              [t1] #
+        #####################################
+
+        """
+        ...
+    
+    def write_page(self):
+        self.pd_lower = 32 # doesnt have
+        self.pd_upper = PAGE_SIZE - 1 - 255 - 16 # doesnt have
+
+        bpd_lower = self.pd_lower.to_bytes(16, byteorder=sys.byteorder)
+        bpd_upper = self.pd_upper.to_bytes(16, byteorder=sys.byteorder)
+
+        with open('../../../.db/page.pg', 'wb') as file:
+            file.seek(PAGE_SIZE - 1)
+            file.write(b'\0')
+            file.close()
+        
+        with open('../../../.db/page.pg', 'wb') as file:
+            file.seek(0)
+            file.write(bpd_lower)
+            
+            file.seek(16)
+            file.write(bpd_upper)
+            
+            file.seek(32)
+            file.write(int(PAGE_SIZE - 1).to_bytes(16, byteorder=sys.byteorder))
+
+            file.seek(PAGE_SIZE - 1 - 255)
+            file.write(b'table')
+
+            file.seek(PAGE_SIZE - 1 - 255 - 16)
+            file.write(int(1856523).to_bytes(16, byteorder=sys.byteorder))
+
+            file.close()
+
+
+    def read_page(self):
+        with open('../../../.db/page.pg', 'rb') as file:
+            file.seek(0)
+            
+            bpd_lower = file.read(16)
+            self.pd_lower = int.from_bytes(bpd_lower, byteorder=sys.byteorder)
+            print(self.pd_lower)
+            
+            file.seek(16)
+            bpd_upper = file.read(16)
+            self.pd_upper = int.from_bytes(bpd_upper, byteorder=sys.byteorder)
+            print(self.pd_upper)
+
+            file.seek(self.pd_lower)
+            baddr_t1 = file.read(16)
+            addr = int.from_bytes(baddr_t1, byteorder=sys.byteorder)
+            print(addr)
+
+            file.seek(addr - 255 - 16)
+            b_oid = file.read(16)
+            oid = int.from_bytes(b_oid, byteorder=sys.byteorder)
+            print(oid)
+
+            file.seek(addr - 255)
+            b_tname = file.read(255)
+            tname = b_tname.decode('utf-8')
+            print(tname)
+
+
+
+if __name__ == '__main__':
+    HeapPage().read_page()
--- yarnd/storage/file/page.py
+++ yarnd/storage/file/page.py
@@ -1,106 +1,21 @@
-import sys
 
 
-# size of a page in bytes
-PAGE_SIZE = 8192
-
-
-class HeapPage:
-
-    # Loc pointer to the last line pointer
-    pd_lower: int
-    # Loc pointer to the last tuple
-    pd_upper: int
-    # line pointers
-    line_pointers: list
-    # tuples
-    tuples: list
+class Page:
+    """ Standard interface for pages used in the management system.
+    """
 
     def __init__(self):
         pass
 
-    def mount_page():
-        """
-        #####################################
-        #           PAGE STRUCTURE          #
-        #####################################
+    def write_page(self) -> bytes:
+        pass
 
-        # [16 bits]
-        # pd_lower [8 bits]
-        # pd_upper [8 bits]
+    def read_page(self, page_path):
+        pass
 
-        # [size - 16 bits]
-        
-        E.g.: Insert one tuple
-        #####################################
-        # [32] [-1] | [-1]                  #
-        #                                   #
-        #                              [t1] #
-        #####################################
+    @staticmethod
+    def create_page(self, tuples = None) -> Page:
+        pass
 
-        """
-        ...
-    
-    def write_page(self):
-        self.pd_lower = 32 # doesnt have
-        self.pd_upper = PAGE_SIZE - 1 - 255 - 16 # doesnt have
-
-        bpd_lower = self.pd_lower.to_bytes(16, byteorder=sys.byteorder)
-        bpd_upper = self.pd_upper.to_bytes(16, byteorder=sys.byteorder)
-
-        with open('../../../.db/page.pg', 'wb') as file:
-            file.seek(PAGE_SIZE - 1)
-            file.write(b'\0')
-            file.close()
-        
-        with open('../../../.db/page.pg', 'wb') as file:
-            file.seek(0)
-            file.write(bpd_lower)
-            
-            file.seek(16)
-            file.write(bpd_upper)
-            
-            file.seek(32)
-            file.write(int(PAGE_SIZE - 1).to_bytes(16, byteorder=sys.byteorder))
-
-            file.seek(PAGE_SIZE - 1 - 255)
-            file.write(b'table')
-
-            file.seek(PAGE_SIZE - 1 - 255 - 16)
-            file.write(int(1856523).to_bytes(16, byteorder=sys.byteorder))
-
-            file.close()
-
-
-    def read_page(self):
-        with open('../../../.db/page.pg', 'rb') as file:
-            file.seek(0)
-            
-            bpd_lower = file.read(16)
-            self.pd_lower = int.from_bytes(bpd_lower, byteorder=sys.byteorder)
-            print(self.pd_lower)
-            
-            file.seek(16)
-            bpd_upper = file.read(16)
-            self.pd_upper = int.from_bytes(bpd_upper, byteorder=sys.byteorder)
-            print(self.pd_upper)
-
-            file.seek(self.pd_lower)
-            baddr_t1 = file.read(16)
-            addr = int.from_bytes(baddr_t1, byteorder=sys.byteorder)
-            print(addr)
-
-            file.seek(addr - 255 - 16)
-            b_oid = file.read(16)
-            oid = int.from_bytes(b_oid, byteorder=sys.byteorder)
-            print(oid)
-
-            file.seek(addr - 255)
-            b_tname = file.read(255)
-            tname = b_tname.decode('utf-8')
-            print(tname)
-
-
-
-if __name__ == '__main__':
-    HeapPage().read_page()
+    def close_page(self):
+        pass
